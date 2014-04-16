bdbfe683ca92197bb7c0bd78bd290a69e9ddffef
mablack01 <mablack01@aol.com>
2014-04-15 20:11:20 -0400

    Finished documenting, finished the interface, cleaned up. We are all done and ready to go :)

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,63 +1,71 @@
 package com;
 
-import java.awt.BorderLayout;
-import java.awt.Desktop;
 import java.awt.EventQueue;
+import java.awt.Font;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
-import java.net.URI;
 import java.text.SimpleDateFormat;
 import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
-import javax.swing.JFrame;
-import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
 import javax.swing.DefaultComboBoxModel;
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
+import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import javax.swing.JTextField;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import javax.swing.JComboBox;
-import javax.swing.JButton;
+import javax.swing.border.EmptyBorder;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.firefox.FirefoxDriver;
-import org.openqa.selenium.htmlunit.HtmlUnitDriver;
+
+/**
+ * Main class, handles the main interface and data.
+ * @author Miles Black
+ * April 15, 2014
+ */
 
 public class Main extends JFrame implements ActionListener {
 
 	private static final long serialVersionUID = -2268907443031783190L;
-	
-	private JButton saveAccount;
+
+	/**
+	 * Fields
+	 */
 	private JButton loadAccount;
+	private JButton saveAccount;
 	private JButton startBot;
 	private JComboBox <String> period;
+	private JLabel currentStatus;
+	private static JLabel status;
 	private JPanel contentPane;
 	private JTextField email;
 	private JTextField password;
-	
+
+	private static Main frame;
 	private User user = new User();
+	
 	private int selectedPeriod;
-	private JLabel currentStatus;
-	private static JLabel status;
 
 	/**
-	 * Launch the application.
+	 * Launches the application.
 	 */
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
-					Main frame = new Main();
+					frame = new Main();
 					frame.setVisible(true);
 				} catch (Exception e) {
 					e.printStackTrace();
@@ -65,121 +73,161 @@ public class Main extends JFrame implements ActionListener {
 			}
 		});
 	}
-	
+
 
 	/**
-	 * Create the frame.
+	 * Creates the main frame.
 	 */
 	public Main() {
 		setTitle("Library Bot by Miles Black");
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 500, 325);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 		setContentPane(contentPane);
-		
+
 		JLabel emailLabel = new JLabel("Email:");
 		email = new JTextField();
 		email.setColumns(10);
-		
+
 		JLabel passwordLabel = new JLabel("Password:");
 		password = new JTextField();
 		password.setColumns(10);
-		
+
 		JLabel periodLabel = new JLabel("Period:");
 		period = new JComboBox <String> ();
 		period.setModel(new DefaultComboBoxModel <String> (new String[] {"R1", "R2", "R3", "R4", "R6", "R7"}));
 		period.addActionListener(this);
 		period.setActionCommand("chosePeriod");
-		
+
 		saveAccount = new JButton("Save Account");
 		saveAccount.addActionListener(this);
 		saveAccount.setActionCommand("saveAccount");
-		
+
 		loadAccount = new JButton("Load Account");
 		loadAccount.addActionListener(this);
 		loadAccount.setActionCommand("loadAccount");
-		
+
 		startBot = new JButton("Start Bot!");
 		startBot.addActionListener(this);
 		startBot.setActionCommand("startBot");
-		
+
 		currentStatus = new JLabel("Current Status:");
-		
+
 		status = new JLabel("Idle...");
-		
+
+		JLabel instructions = new JLabel("Instructions:");
+
+		JLabel line1 = new JLabel("Make sure that you enter all of your proper information into");
+
+		JLabel line2 = new JLabel("the following fields and click \"Save Account\". Also, make sure");
+
+		JLabel line3 = new JLabel("that you have FireFox installed, this is required to run the");
+
+		JLabel line4 = new JLabel("program.");
+
+		JLabel beatTheSixies = new JLabel("ENJOY BEATING THE SIXIES!");
+		beatTheSixies.setFont(new Font("Tahoma", Font.BOLD, 16));
+
 		GroupLayout gl_contentPane = new GroupLayout(contentPane);
 		gl_contentPane.setHorizontalGroup(
-			gl_contentPane.createParallelGroup(Alignment.TRAILING)
+				gl_contentPane.createParallelGroup(Alignment.TRAILING)
 				.addGroup(gl_contentPane.createSequentialGroup()
-					.addContainerGap()
-					.addComponent(startBot, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
-					.addGap(41)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-						.addComponent(currentStatus)
-						.addComponent(status))
-					.addPreferredGap(ComponentPlacement.RELATED, 41, Short.MAX_VALUE)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
-						.addComponent(password, Alignment.LEADING)
-						.addComponent(loadAccount, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(saveAccount, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
-						.addComponent(periodLabel, Alignment.LEADING)
-						.addComponent(passwordLabel, Alignment.LEADING)
-						.addComponent(emailLabel, Alignment.LEADING)
-						.addComponent(email, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
-						.addComponent(period, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
-					.addContainerGap())
-		);
-		gl_contentPane.setVerticalGroup(
-			gl_contentPane.createParallelGroup(Alignment.LEADING)
-				.addGroup(gl_contentPane.createSequentialGroup()
-					.addGap(18)
-					.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING)
-						.addGroup(gl_contentPane.createSequentialGroup()
-							.addComponent(emailLabel)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(email, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(passwordLabel)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addGap(11)
-							.addComponent(periodLabel)
-							.addPreferredGap(ComponentPlacement.RELATED)
-							.addComponent(period, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(saveAccount)
-							.addPreferredGap(ComponentPlacement.UNRELATED)
-							.addComponent(loadAccount))
+						.addContainerGap()
 						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
-							.addGroup(gl_contentPane.createSequentialGroup()
-								.addComponent(currentStatus)
-								.addPreferredGap(ComponentPlacement.RELATED)
-								.addComponent(status))
-							.addComponent(startBot, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)))
-					.addContainerGap(13, Short.MAX_VALUE))
-		);
+								.addGroup(gl_contentPane.createSequentialGroup()
+										.addComponent(startBot, GroupLayout.PREFERRED_SIZE, 120, GroupLayout.PREFERRED_SIZE)
+										.addGap(18)
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+												.addComponent(currentStatus, GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)
+												.addComponent(status, GroupLayout.DEFAULT_SIZE, 183, Short.MAX_VALUE)))
+												.addComponent(instructions)
+												.addComponent(line1)
+												.addComponent(line2)
+												.addComponent(line3)
+												.addComponent(line4)
+												.addComponent(beatTheSixies))
+												.addPreferredGap(ComponentPlacement.RELATED)
+												.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+														.addComponent(password, Alignment.LEADING)
+														.addComponent(loadAccount, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+														.addComponent(saveAccount, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+														.addComponent(periodLabel, Alignment.LEADING)
+														.addComponent(passwordLabel, Alignment.LEADING)
+														.addComponent(emailLabel, Alignment.LEADING)
+														.addComponent(email, Alignment.LEADING, GroupLayout.DEFAULT_SIZE, 127, Short.MAX_VALUE)
+														.addComponent(period, Alignment.LEADING, 0, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+														.addContainerGap())
+				);
+		gl_contentPane.setVerticalGroup(
+				gl_contentPane.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_contentPane.createSequentialGroup()
+						.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+								.addGroup(gl_contentPane.createSequentialGroup()
+										.addGap(18)
+										.addGroup(gl_contentPane.createParallelGroup(Alignment.TRAILING, false)
+												.addGroup(gl_contentPane.createSequentialGroup()
+														.addComponent(emailLabel)
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addComponent(email, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addComponent(passwordLabel)
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addComponent(password, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addGap(11)
+														.addComponent(periodLabel)
+														.addPreferredGap(ComponentPlacement.RELATED)
+														.addComponent(period, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addComponent(saveAccount)
+														.addPreferredGap(ComponentPlacement.UNRELATED)
+														.addGroup(gl_contentPane.createParallelGroup(Alignment.BASELINE)
+																.addComponent(loadAccount)
+																.addComponent(status)))
+																.addGroup(gl_contentPane.createSequentialGroup()
+																		.addGap(13)
+																		.addComponent(line1)
+																		.addPreferredGap(ComponentPlacement.RELATED)
+																		.addComponent(line2)
+																		.addPreferredGap(ComponentPlacement.RELATED)
+																		.addComponent(line3)
+																		.addPreferredGap(ComponentPlacement.RELATED)
+																		.addComponent(line4)
+																		.addGap(36)
+																		.addComponent(beatTheSixies)
+																		.addPreferredGap(ComponentPlacement.RELATED, GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
+																		.addGroup(gl_contentPane.createParallelGroup(Alignment.LEADING)
+																				.addComponent(currentStatus)
+																				.addComponent(startBot, GroupLayout.PREFERRED_SIZE, 43, GroupLayout.PREFERRED_SIZE)))))
+																				.addGroup(gl_contentPane.createSequentialGroup()
+																						.addContainerGap()
+																						.addComponent(instructions)))
+																						.addContainerGap(38, Short.MAX_VALUE))
+				);
 		contentPane.setLayout(gl_contentPane);
 	}
-	
+
+	/**
+	 * Handles the actions performed in the main frame.
+	 */
 	@Override
 	public void actionPerformed(ActionEvent e) {
-	   if (e.getActionCommand().equals("chosePeriod")) {
-	      		@SuppressWarnings("unchecked")
-	      	JComboBox <String> period = (JComboBox <String>)e.getSource();
-	      	if (period.getSelectedIndex() == 0)
-	    	    selectedPeriod = 1;
-	      	else if (period.getSelectedIndex() == 1)
-	      		selectedPeriod = 2;
-	      	else if (period.getSelectedIndex() == 2)
-	      		selectedPeriod = 3;
-	      	else if (period.getSelectedIndex() == 3)
-	      		selectedPeriod = 4;
-	      	else if (period.getSelectedIndex() == 4)
-	      		selectedPeriod = 6;
-	      	else if (period.getSelectedIndex() == 5)
-	      		selectedPeriod = 7;
-	   } else if (e.getActionCommand().equals("saveAccount")) {
+		if (e.getActionCommand().equals("chosePeriod")) {
+			@SuppressWarnings("unchecked")
+			JComboBox <String> period = (JComboBox <String>)e.getSource();
+			if (period.getSelectedIndex() == 0)
+				selectedPeriod = 1;
+			else if (period.getSelectedIndex() == 1)
+				selectedPeriod = 2;
+			else if (period.getSelectedIndex() == 2)
+				selectedPeriod = 3;
+			else if (period.getSelectedIndex() == 3)
+				selectedPeriod = 4;
+			else if (period.getSelectedIndex() == 4)
+				selectedPeriod = 6;
+			else if (period.getSelectedIndex() == 5)
+				selectedPeriod = 7;
+		} else if (e.getActionCommand().equals("saveAccount")) {
 			if (user == null) {
 				user = new User(email.getText(), password.getText(), selectedPeriod);
 			} else {
@@ -192,70 +240,83 @@ public class Main extends JFrame implements ActionListener {
 				out.writeObject(user);
 				out.close();
 			} catch(Throwable e1) {
-				
+
 			}
-	   } else if (e.getActionCommand().equals("loadAccount")) {
+		} else if (e.getActionCommand().equals("loadAccount")) {
 			try {
 				ObjectInputStream in = new ObjectInputStream(new FileInputStream("account.ser"));
 				user = (User) in.readObject();
 				in.close();
 			} catch(Throwable e1) {
-				
+
 			}
 			email.setText(user.getEmail());
 			password.setText(user.getPassword());
-	      	if (user.getPeriod() == 1)
-	    	    period.setSelectedIndex(0);
-	      	else if (user.getPeriod() == 2)
-	      		period.setSelectedIndex(1);
-	      	else if (user.getPeriod() == 3)
-	      		period.setSelectedIndex(2);
-	      	else if (user.getPeriod() == 4)
-	      		period.setSelectedIndex(3);
-	      	else if (user.getPeriod() == 6)
-	      		period.setSelectedIndex(4);
-	      	else if (user.getPeriod() == 7)
-	      		period.setSelectedIndex(5);
-	   } else if (e.getActionCommand().equals("startBot")) {
-		   if (user == null) {
-			   //insert error
-			   return;
-		   }
-		   Timer timer = new Timer();
-		    timer.schedule( new TimerTask() {
-		      public void run() {
-		        if (getTime().contains("7:01")) {
-		            try {
-		   				WebDriver driver = new FirefoxDriver();            
-		   				driver.get("https://pickatime.com/client?ven=11607876&login=true&email="+user.getEmail()+"&password="+user.getPassword()+"");
-		   				String time = getTime(user.getPeriod());
-		   				driver.findElement(By.linkText(time)).click();
-		   				driver.findElement(By.name("bookIt")).click();
-		   				driver.findElement(By.name("ret")).click();
-		   				setStatus("Successfully signed up for the library!");
-		   				cancel();
-		              } catch(Exception ex) {
-		                ex.printStackTrace();
-		              }
-		        } else if (getTime().contains("7:00")) {
-		        	setStatus("It is now 7PM, preparing to sign you up...");
-		        } else {
-		        	setStatus("Waiting for it to be 7PM...");
-		        }
-		      }
-		    }, 0, 60*1000);
-	   }
+			if (user.getPeriod() == 1)
+				period.setSelectedIndex(0);
+			else if (user.getPeriod() == 2)
+				period.setSelectedIndex(1);
+			else if (user.getPeriod() == 3)
+				period.setSelectedIndex(2);
+			else if (user.getPeriod() == 4)
+				period.setSelectedIndex(3);
+			else if (user.getPeriod() == 6)
+				period.setSelectedIndex(4);
+			else if (user.getPeriod() == 7)
+				period.setSelectedIndex(5);
+		} else if (e.getActionCommand().equals("startBot")) {
+			if (user.getEmail() == null || user.getPassword() == null || user.getPeriod() <= -1) {
+				JOptionPane.showMessageDialog(frame, "Please correctly fill out your information.");
+				return;
+			}
+			Timer timer = new Timer();
+			timer.schedule( new TimerTask() {
+				public void run() {
+					if (getTime().contains("7:01")) {
+						try {
+							WebDriver driver = new FirefoxDriver();            
+							driver.get("https://pickatime.com/client?ven=11607876&login=true&email="+user.getEmail()+"&password="+user.getPassword()+"");
+							String time = getTime(user.getPeriod());
+							driver.findElement(By.linkText(time)).click();
+							driver.findElement(By.name("bookIt")).click();
+							driver.findElement(By.name("ret")).click();
+							setStatus("Successfully signed up for the library!");
+							cancel();
+						} catch(Exception ex) {
+							ex.printStackTrace();
+						}
+					} else if (getTime().contains("7:00")) {
+						setStatus("It is now 7PM, preparing to sign you up...");
+					} else {
+						setStatus("Waiting for it to be 7PM...");
+					}
+				}
+			}, 0, 60*1000);
+		}
 	}
-	
+
+	/**
+	 * Grabs the time in the proper format for the bot to check.
+	 * @return The local computer time.
+	 */
 	public static String getTime() { 
 		SimpleDateFormat format = new SimpleDateFormat("h:mm"); 
 		return format.format(new Date());
 	}
-	
+
+	/**
+	 * Sets the status for the main frame.
+	 * @param state The current status for the main frame.
+	 */
 	public static void setStatus(String state) {
 		status.setText(state);
 	}
-	
+
+	/**
+	 * Grabs the text to search for on the sign up page.
+	 * @param period The user's period to determine which time to select.
+	 * @return The String that Selenium searches for on the page.
+	 */
 	public static String getTime(int period) {
 		switch(period) {
 		case 1:
@@ -273,5 +334,4 @@ public class Main extends JFrame implements ActionListener {
 		}
 		return null;
 	}
-
 }
--- src/com/User.java
+++ src/com/User.java
@@ -1,47 +1,87 @@
 package com;
+
 import java.io.Serializable;
 
+/**
+ * User class, handles the user settings
+ * @author Miles Black
+ * April 15, 2014
+ */
 
 public class User implements Serializable {
 	
 	private static final long serialVersionUID = 5647469103804189021L;
 	
+	/**
+	 * Fields
+	 */
 	private String email;
 	private String password;
 	private int period;
 	
+	/**
+	 * Default Constructor
+	 */
 	public User() {
-		this.email = "name@email.com";
-		this.password = "password";
-		this.period = 1;
+
 	}
 	
+	/**
+	 * User Constructor
+	 * @param email Assigns the given email to the user.
+	 * @param password Assigns the given password to the user.
+	 * @param period Indictates the period that the user is signing up for.
+	 */
 	public User(String email, String password, int period) {
 		this.email = email;
 		this.password = password;
 		this.period = period;
 	}
 	
+	/**
+	 * Gets the user's email
+	 * @return user's email
+	 */
 	public String getEmail() {
 		return email;
 	}
 	
+	/**
+	 * Gets the user's password
+	 * @return user's password
+	 */
 	public String getPassword() {
 		return password;
 	}
 	
+	/**
+	 * Gets the user's period
+	 * @return user's period
+	 */
 	public int getPeriod() {
 		return period;
 	}
 	
+	/**
+	 * Sets the user's email
+	 * @param email The email that is assigned to the user
+	 */
 	public void setEmail(String email) {
 		this.email = email;
 	}
 	
+	/**
+	 * Sets the user's password
+	 * @param password The password that is assigned to the user
+	 */
 	public void setPassword(String password) {
 		this.password = password;
 	}
 	
+	/**
+	 * Sets the user's period
+	 * @param period The period that is assigned to the user
+	 */
 	public void setPeriod(int period) {
 		this.period = period;
 	}
